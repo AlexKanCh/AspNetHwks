326aa33c4cb1610a81514b52fb633a66768341a9
KucherovAN <alexander.n.kan@gmail.com>
2025-04-26 23:11:36 +0300

    use grpc client

--- Homeworks/RabbitMQ/src/Pcf.ReceivingFromPartner/Pcf.ReceivingFromPartner.Integration/GivingPromoCodeToCustomerGateway.cs
+++ Homeworks/RabbitMQ/src/Pcf.ReceivingFromPartner/Pcf.ReceivingFromPartner.Integration/GivingPromoCodeToCustomerGateway.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using Grpc.Core;
 using Grpc.Net.Client;
+using Pcf.Grpc.Contracts;
 using Pcf.ReceivingFromPartner.Core.Abstractions.Gateways;
 using Pcf.ReceivingFromPartner.Core.Domain;
 
@@ -11,36 +12,36 @@ namespace Pcf.ReceivingFromPartner.Integration
     public class GivingPromoCodeToCustomerGateway
         : IGivingPromoCodeToCustomerGateway
     {
-        //private readonly PromoCodeService.PromoCodeServiceClient _client;
+        private readonly PromoCodeService.PromoCodeServiceClient _client;
         public GivingPromoCodeToCustomerGateway(string grpcServerUrl)
         {
-            //var channel = GrpcChannel.ForAddress(grpcServerUrl);
-            //_client = new PromoCodeService.PromoCodeServiceClient(channel);
+            var channel = GrpcChannel.ForAddress(grpcServerUrl);
+            _client = new PromoCodeService.PromoCodeServiceClient(channel);
         }
 
         public async Task GivePromoCodeToCustomer(PromoCode promoCode)
         {
-            //var request = new PromoCodeRequest()
-            //{
-            //    PartnerId = promoCode.Partner.Id,
-            //    BeginDate = promoCode.BeginDate.ToShortDateString(),
-            //    EndDate = promoCode.EndDate.ToShortDateString(),
-            //    PreferenceId = promoCode.PreferenceId,
-            //    PromoCode = promoCode.Code,
-            //    ServiceInfo = promoCode.ServiceInfo,
-            //    PartnerManagerId = promoCode.PartnerManagerId
-            //};
+            var request = new PromoCodeRequest()
+            {
+                PartnerId = promoCode.Partner.Id.ToString(),
+                BeginDate = new DateTimeOffset(promoCode.BeginDate).ToUnixTimeMilliseconds(),
+                EndDate = new DateTimeOffset(promoCode.EndDate).ToUnixTimeMilliseconds(),
+                PreferenceId = promoCode.PreferenceId.ToString(),
+                Code = promoCode.Code,
+                ServiceInfo = promoCode.ServiceInfo,
+                PartnerManagerId = promoCode.PartnerManagerId.ToString()
+            };
 
-            //try
-            //{
-            //    await _client.CreatePromoCodeAsync(request);
+            try
+            {
+                await _client.CreatePromoCodeAsync(request);
 
-            //    Console.WriteLine("Promo code created successfully.");
-            //}
-            //catch (RpcException ex)
-            //{
-            //    Console.WriteLine($"gRPC error: {ex.Status.Detail}");
-            //}
+                Console.WriteLine("Promo code created successfully.");
+            }
+            catch (RpcException ex)
+            {
+                Console.WriteLine($"gRPC error: {ex.Status.Detail}");
+            }
         }
     }
 }
